aa990dd81e1d167e429915af30f2b88e16da053d
Ajayrajc1998 <54671682+Ajayrajc1998@users.noreply.github.com>
2023-10-15 20:31:00 +0530

    Time: 112 ms (11.49%), Space: 13.2 MB (90.8%) - LeetHub

---
+++ 2585-delete-greatest-value-in-each-row/2585-delete-greatest-value-in-each-row.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def deleteGreatestValue(self, grid):
+        """
+        :type grid: List[List[int]]
+        :rtype: int
+        """
+        i=0
+        res=0
+        while grid!=[[] for _ in range(len(grid))]:
+           gp=[]
+           while i<len(grid):
+               gp.append(max(grid[i]))
+               grid[i].remove(max(grid[i]))
+               i+=1
+           print(gp)
+           res+=max(gp)
+           i=0
+        return res      
