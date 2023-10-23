a00f78024bb7fc5f2c0a13ffce5517ecc1772de2
Ajayrajc1998 <54671682+Ajayrajc1998@users.noreply.github.com>
2023-10-23 11:26:33 +0530

    Time: 43 ms (17.79%), Space: 13.6 MB (11.96%) - LeetHub

---
+++ 1570-final-prices-with-a-special-discount-in-a-shop/1570-final-prices-with-a-special-discount-in-a-shop.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def finalPrices(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: List[int]
+        """
+        res=[]
+        for i in range(len(prices)):
+            flag=False
+            for j in range(i+1,len(prices)):
+                if prices[i]>=prices[j]:
+                    print("Yess")
+                    res.append(prices[i]-prices[j])
+                    flag=True
+                    break
+            if flag==False:
+                res.append(prices[i])
+                
+        return res
