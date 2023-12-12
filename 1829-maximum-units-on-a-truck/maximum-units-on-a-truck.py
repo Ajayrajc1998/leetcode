0287050e3003df51912915e96975a6c8a1dbff8e
Ajayrajc1998 <54671682+Ajayrajc1998@users.noreply.github.com>
2023-12-12 12:49:47 +0530

    Time: 139 ms (21.16%) | Memory: 13.9 MB (32.78%) - LeetSync

--- 1829-maximum-units-on-a-truck/maximum-units-on-a-truck.py
+++ 1829-maximum-units-on-a-truck/maximum-units-on-a-truck.py
@@ -7,25 +7,18 @@ class Solution(object):
         """
         target=0
         result=0
-        boxTypes=sorted_data = sorted(boxTypes, key=lambda x: x[1],reverse=True)
-        print('sorted box',boxTypes)
-        
+        boxTypes=sorted_data = sorted(boxTypes, key=lambda x: x[1],reverse=True) 
         for i in range(len(boxTypes)):
             print(boxTypes[i])
             if boxTypes[i][0]+target<=truckSize:
                 target+=boxTypes[i][0]
                 result+=(boxTypes[i][0]*boxTypes[i][1])
-                print('filling',boxTypes[i][0],boxTypes[i][1])
-            
             elif target==truckSize:
-                print('filled')
                 break
             elif boxTypes[i][0]+target>truckSize:
                 remain=(truckSize-target)
                 result+=(remain*boxTypes[i][1])
-                print('over filling',remain,boxTypes[i][1])
                 break
-            print('target achievedf',target)
-            print("-------------------------")
+         
         return result
                 
