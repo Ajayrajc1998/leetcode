5adf8b0de044a0be8df6e3847e4d2b8d3ce5df4f
Ajayrajc1998 <54671682+Ajayrajc1998@users.noreply.github.com>
2024-01-04 17:45:46 +0530

    Time: 16 ms (54.89%) | Memory: 13.2 MB (50.75%) - LeetSync

---
+++ 1552-build-an-array-with-stack-operations/build-an-array-with-stack-operations.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def __init__(self):
+        self.stack=[]
+    def buildArray(self, target, n):
+        """
+        :type target: List[int]
+        :type n: int
+        :rtype: List[str]
+        """
+        result=[]
+        checker=[i for i in range(1,n+1)]
+        print(checker)
+        i=0
+        count=0
+        while True:
+            if checker[i] in target:
+                result.append('Push')
+                count+=1
+            else:
+                result.append('Push')
+                result.append("Pop")
+            i+=1
+            if count==len(target):
+                return result 
+
+        
