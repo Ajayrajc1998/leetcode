8e32ec6da5476e2a343356391c2c41c4cee61576
Ajayrajc1998 <54671682+Ajayrajc1998@users.noreply.github.com>
2024-09-29 13:25:31 +0530

    Time: 79 ms (5.77%) | Memory: 11.5 MB (93.47%) - LeetSync

---
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def check(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        if nums==sorted(nums):
+            return True
+        else:
+            rotate=0
+            while rotate<len(nums):
+                val=nums.pop()
+                nums.insert(0,val)
+                print(nums)
+                if nums==sorted(nums):
+                    return True
+                rotate+=1
+        
+        return False
