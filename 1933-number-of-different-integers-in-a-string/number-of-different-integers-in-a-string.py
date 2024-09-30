85510cafa22ad1c861603075eddaba49b30ccad8
Ajayrajc1998 <54671682+Ajayrajc1998@users.noreply.github.com>
2024-09-30 14:11:43 +0530

    Time: 29 ms (8.11%) | Memory: 11.7 MB (53.15%) - LeetSync

---
+++ 1933-number-of-different-integers-in-a-string/number-of-different-integers-in-a-string.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def numDifferentIntegers(self, word):
+        """
+        :type word: str
+        :rtype: int
+        """
+        dig=set()
+        dig_flag=False
+        num=''
+        for i in range(len(word)):
+            if word[i].isdigit() and not dig_flag:
+                dig_flag=True
+            if word[i].isdigit() and dig_flag:
+                num+=word[i]
+            if not word[i].isdigit() and dig_flag:
+                dig.add(int(num))
+                dig_flag=False
+                num=''
+            print(num)
+        if not word.isalpha() and dig_flag:
+            dig.add(int(num))
+        print(dig)
+        return len(dig)
+                
